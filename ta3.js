647539d562e939b0072d9f4fac575beee2ef8cee
Pavithra241997 <124482806+Pavithra241997@users.noreply.github.com>
2023-05-22 17:24:27 +0530

    Add files via upload

---
+++ ta3.js
@@ -0,0 +1,13 @@
+var num1=24;
+var num2=20;
+if(num1>num2){
+    console.log("num1 is the largest");
+}
+else if(num1<num2)
+{
+  console.log("num2 is  the largest");
+}
+else
+{
+    console.log( "num1 num2 is equal")
+}
