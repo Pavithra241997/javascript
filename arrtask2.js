8a5b98eb390ef930a7cf2bd760e117dce1636c40
Pavithra241997 <124482806+Pavithra241997@users.noreply.github.com>
2023-05-22 17:33:43 +0530

    Add files via upload

---
+++ arrtask2.js
@@ -0,0 +1,18 @@
+arr=[25,false,"green","apple"];
+console.log(arr[0]);
+
+var arr=[25,50,"center",75,100];
+arr.shift(25);
+arr.unshift(5);
+console.log(arr);
+
+var arr=[12,23,"KGM",45,56,67];
+console.log(arr.length);
+
+var arr=[1,2,4];
+arr.push(100);
+console.log(arr);
+
+var arr=[1,2,3,4,"KGM","empty"];
+arr.pop();
+console.log(arr);
