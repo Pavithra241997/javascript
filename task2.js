fd9e3ad03eec8dc7cad58aaf1a3e2d593a3ec556
Pavithra241997 <124482806+Pavithra241997@users.noreply.github.com>
2023-05-22 18:06:10 +0530

    Add files via upload

--- task2.js
+++ task2.js
@@ -1,23 +1,15 @@
-// var model1={
-//     name:'Mercedes Benz',
-//     model:'C-Class',
-//     manufacturer:'Daimler Ag company',
-//     length:'4686mm',
-//     cargo_capacity:'12.6 cubic feet'
+var anchoviesPizzaScore = 0;
+var  pepperoniPizzaScore = 4;
+var  hawaiianPizzaScore = 4;
+var  chickenBaconRanchPizzaScore = 6;
 
-// }
-// console.log(model1);
+ console.log(anchoviesPizzaScore < pepperoniPizzaScore)
 
-var act={
-    title:'Baahubali',
-    director:'Rajamouli',
-    producers:['Sobhu','Reghavendra','Prasad'],
-    'laed actors with their roles':{
-        Prabhas:'Baaghubali',
-        anushaka:'Devasena',
-        rana:'Bhalladeva',
-        tamanna:'Avanthika'
-    }
-}
-console.log(act);
+ console.log(chickenBaconRanchPizzaScore > hawaiianPizzaScore)
+
+ console.log(hawaiianPizzaScore > pepperoniPizzaScore)
+
+ console.log(pepperoniPizzaScore <= chickenBaconRanchPizzaScore)
+
+ console.log(hawaiianPizzaScore >= pepperoniPizzaScore)
 
